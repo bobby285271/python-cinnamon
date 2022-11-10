ff385091cf56bdf37fbede117025d41e9786507b
Bobby Rong <rjl931189261@126.com>
2022-11-10 19:47:01 +0800

    5.4.12

---
+++ README.md
@@ -0,0 +1,3 @@
+### python-cinnamon
+
+https://github.com/NixOS/nixpkgs/issues/200397
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="python-cinnamon",
+    version="5.4.12",
+    description="Python Cinnamon Library",
+    maintainer="Bobby Rong",
+    maintainer_email="rjl931189261@126.com",
+    url="http://github.com/bobby285271/python-cinnamon",
+    packages=['cinnamon'],
+    classifiers=[
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Desktop Environment",
+    ],
+)
